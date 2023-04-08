24963ce40fa49f93ffafb855cb050946598793f5
AlexGrigorenco <alpsnhandy@gmail.com>
2023-04-09 00:56:32 +0300

    seting value and color to tile

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const gameBoard = document.getElementById('game-board')
 
 const grid = new Grid(gameBoard)
 grid.getRandomEmptyCell().linkTile(new Tile(gameBoard))
-
+grid.getRandomEmptyCell().linkTile(new Tile(gameBoard))
 
 
 
