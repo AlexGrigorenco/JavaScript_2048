91f6af431bae87496d53f9a0968cdd7db3d89418
AlexGrigorenco <alpsnhandy@gmail.com>
2023-04-11 11:59:22 +0300

    added animation join

--- tile.js
+++ tile.js
@@ -33,4 +33,10 @@ export class Tile{
     removeFromDOM(){
         this.tileElement.remove()
     }
+
+    waitForTransitionEnd(){
+        return new Promise(resolve => {
+            this.tileElement.addEventListener('transitionend', resolve, {once: true})
+        })
+    }
 }
